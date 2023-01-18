a61111a2bee020887044c9c302289dc419198df0
joesurf <desictiverk@gmail.com>
2023-01-18 16:27:04 +0800

    Upgrade bot version

--- main.py
+++ main.py
@@ -5,26 +5,43 @@ from pathlib import Path
 from pprint import pprint
 from dotenv import load_dotenv
 
-from telegram.ext.updater import Updater
-from telegram.update import Update
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.messagehandler import MessageHandler
-from telegram.ext.filters import Filters
+
+from telegram import __version__ as TG_VER
+
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+
+if __version_info__ < (20, 0, 0, "alpha", 1):
+    raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+        f"{TG_VER} version of this example, "
+        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+    )
+from telegram import LabeledPrice, ShippingOption, Update
 from telegram.ext import (
+    Application,
     CommandHandler,
-    ConversationHandler,
+    ContextTypes,
     MessageHandler,
+    PreCheckoutQueryHandler,
+    ShippingQueryHandler,
     filters,
 )
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 
-def start(update: Update, context: CallbackContext):
+def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """
     """
     update.message.reply_text(
@@ -36,19 +53,17 @@ def start(update: Update, context: CallbackContext):
 
 
 def main() -> None:
-    """
-    Run the telegram bot.
-    """
+    """Run the bot."""
     # Create the Application and pass it your bot's token.
-    updater = Updater(
-        os.environ.get("TELEGRAM_BOT_TOKEN"),
-        use_context=True
-    )
+    application = Application.builder().token(os.environ.get("TELEGRAM_BOT_TOKEN")).build()
 
-    updater.dispatcher.add_handler(CommandHandler('start', start))
+    # simple start function
+    application.add_handler(CommandHandler("start", start))
+
 
     # Run the bot until the user presses Ctrl-C
-    updater.start_polling()
+    application.run_polling()
 
 
-main()
+if __name__ == "__main__":
+    main()
